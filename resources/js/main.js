d63dceac869a302bf293f4dc6d8fffe3d7671b0f
chelsea-lightfoot <clightfootdesign@gmail.com>
2020-04-24 20:06:53 -0700

    Setup array for items

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,43 +2,19 @@ $(document).ready(function() {
 
     console.log('beep beep imma sheep');
 
+    var itemDatabase = [
+        {
+            name: "Item Name",
+            property: "Properties",
+            description: "Description",
+        },
+        {
+            name: "Item Name 2",
+            property: "Properties 2",
+            description: "Description 2",
+        },
+    ];
 
-    //SMOOTH SCROLLING
-    $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#!"]')
-        .click(function(event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
-            }
-        });
-    //END SMOOTH SCROLLING STARTED ABOVE (Yes it's closed, Future Chelsea, I promise)
+    console.log(itemDatabase[1].property);
 
 });
