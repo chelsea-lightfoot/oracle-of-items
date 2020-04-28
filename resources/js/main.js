6f42a9f5a9dff3baf944d5f73a43f26cb67add9b
chelsea-lightfoot <clightfootdesign@gmail.com>
2020-04-28 13:51:42 -0700

    Set up generation and printing of object values

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,20 +1,69 @@
 $(document).ready(function() {
-
-    console.log('beep beep imma sheep');
-
     var itemDatabase = [
         {
-            name: "Item Name",
-            property: "Properties",
-            description: "Description",
+            name: "Item Name 1",
+            property: "Properties 1",
+            description: "Description 1",
         },
         {
             name: "Item Name 2",
             property: "Properties 2",
             description: "Description 2",
         },
+        {
+            name: "Item Name 3",
+            property: "Properties 3",
+            description: "Description 3",
+        },
+        {
+            name: "Item Name 4",
+            property: "Properties 4",
+            description: "Description 4",
+        },
+        {
+            name: "Item Name 5",
+            property: "Properties 5",
+            description: "Description 5",
+        },
+        {
+            name: "Item Name 6",
+            property: "Properties 6",
+            description: "Description 6",
+        },
+        {
+            name: "Item Name 7",
+            property: "Properties 7",
+            description: "Description 7",
+        },
+        {
+            name: "Item Name 8",
+            property: "Properties 8",
+            description: "Description 8",
+        },
+        {
+            name: "Item Name 9",
+            property: "Properties 9",
+            description: "Description 9",
+        },
+        {
+            name: "Item Name 10",
+            property: "Properties 10",
+            description: "Description 10",
+        },
     ];
 
-    console.log(itemDatabase[1].property);
+    var databaseSize = itemDatabase.length;
+
+    $('#runGen').click(function() {
+
+        var chosenItem = [Math.floor(Math.random() * databaseSize)];
+
+        $('#itemCard p.name').replaceWith('<p class="name">' + itemDatabase[chosenItem].name + '</p>');
+
+        $('#itemCard p.property').replaceWith('<p class="property">' + itemDatabase[chosenItem].property + '</p>');
+
+        $('#itemCard p.description').replaceWith('<p class="description">' + itemDatabase[chosenItem].description + '</p>');
+
+    });
 
 });
